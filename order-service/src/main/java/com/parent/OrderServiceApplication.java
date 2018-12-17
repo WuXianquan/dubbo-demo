f615198be363a0ec9cdc994bdfefd4bb23890a87
WuXianquan <lonquan@foxmail.com>
2018-12-17 11:18:29 +0800

    添加本地存根测试

--- order-service/src/main/java/com/parent/OrderServiceApplication.java
+++ order-service/src/main/java/com/parent/OrderServiceApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableCaching
 @EnableDubbo
+@EnableRedisHttpSession
 @SpringBootApplication
 public class OrderServiceApplication extends SpringBootServletInitializer {
 
--- order-service/src/main/java/com/parent/controller/TestController.java
+++ order-service/src/main/java/com/parent/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.parent.controller;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,10 +16,25 @@ public class TestController {
 
     /**
      * 测试tomcat部署 http://localhost:8081/order/test/success
+     *
      * @return
      */
     @RequestMapping("/success")
-    public String test(){
+    public String test() {
         return "test success!";
+    }
+
+    @RequestMapping("/testSetRedis")
+    public String testSetRedis(HttpServletRequest request, @RequestParam(defaultValue = "Lon") String username) {
+        String key = "user_" + username;
+        request.setAttribute(key, username);
+        return key;
+    }
+
+    @RequestMapping("/testGetRedis")
+    public String testGetRedis(HttpServletRequest request, @RequestParam(defaultValue = "Lon") String username) {
+        String key = "user_" + username;
+        System.out.println(request.getAttribute(key));
+        return (String) request.getAttribute(key);
     }
 }
